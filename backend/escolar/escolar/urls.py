384279eb74e678a5433ef10bdb7a58f601959887
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2026-06-17 18:36:07 +0000

    fix: remover imports não usados

--- backend/escolar/escolar/urls.py
+++ backend/escolar/escolar/urls.py
@@ -17,13 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from rest_framework import permissions
-from rest_framework.routers import DefaultRouter
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from api import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
